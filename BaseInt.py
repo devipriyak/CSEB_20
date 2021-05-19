d74dd2618433958b80c1838dccd800406811e45a
devipriyak <k.devipriya20@gmail.com>
2021-05-19 15:20:26 +0530

    Add files via upload

---
+++ BaseInt.py
@@ -0,0 +1,62 @@
+#int() Base
+#int(String)
+#int(String,base=10)
+#Numbers Representation
+#1234-Decimal ,Base-10
+#1010---Binary Representation-Base-2
+#5-Octal Representation,Base-8
+#AF1-Hex Representation,Base -16
+print('Base 10 Representation')
+a=int("1001",10)#Base 10 Representation
+print(a)
+print('Base-2 Representation')
+a=int("1001111",base=2)
+#The given parameter is consider as
+#binary format-Now int() function converts
+#the binary value into an integer
+print(a)#4
+print("Hex Representation")
+a=int("F",base=16)#15
+print(a)
+#a=int("G",base=16)
+#print(a)#ValueError:Base 16 -A to F only Acceptable
+print('Octal Representation')
+a=int("11",base=8)
+print(a)
+#101----   8^2+8^0 =65
+#11-----     8^1 +8^0=9
+Task1: for conversions-Built in functions
+#integer to Binary
+#integer to Hex
+#integer to octal
+
+Task2:int,float,str,bool --data types
+What is the size of each data type-How many bytes
+#Task3 :1.1 Lab Experiment
+Aim:
+      Source code
+      Expected Output:
+
+      Actual Output>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
