3c9ce3af97b8acb0f26e3af4973fa446d9fb9a81
devipriyak <k.devipriya20@gmail.com>
2021-05-31 10:25:36 +0530

    Add files via upload

---
+++ forsimple.py
@@ -0,0 +1,28 @@
+print(range(10))#[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+end=10
+for i in range(end):#i=0,
+      print(i)
+
+#Explicitly specifying start and end values
+print("Explicitly specifying start and end values")
+start=5
+end=12
+for i in range(5,12):#5,6,..11(Default step count is 1)
+      print i,#python 3-print(i,end=" ")
+
+print("\n")#Blank empty line
+for i in range(5,20,3):#three parameters start,end,stepcount
+      print i,#5,8,11,14,17(20 not printed)
+
+
+
+
+
+
+'''How the for loop i python working
+Initialization
+Condition Checking
+Increment of loop
+intilialization i=0,i<10
+then loop is executed,increment of i
+'''
